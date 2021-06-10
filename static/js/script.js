edc2359fab6b821f2705b7770e0feaae728e71b0
adey-px <aosteph@gmail.com>
2021-06-10 19:37:07 +0000

    Add validation to new_employee form

--- static/js/script.js
+++ static/js/script.js
@@ -1,14 +1,49 @@
-// Table search functionality for employee directory
 $(document).ready(function(){
+  // Table search functionality for employee directory
     $("#tableSearch").on("keyup", function() {
       var value = $(this).val().toLowerCase();
       $("#myTable tr").filter(function() {
         $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
       });
     });
-  });
 // Search functionality for employee directory ends
+
+// New employee form inputs validation
+    (function() {
+      'use strict';
+      window.addEventListener('load', function() {
+      // Fetch all the forms we want to apply custom Bootstrap validation styles to
+      var forms = document.getElementsByClassName('needs-validation');
+      // Loop over them and prevent submission
+      var validation = Array.prototype.filter.call(forms, function(form) {
+      form.addEventListener('submit', function(event) {
+      if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+      }
+      form.classList.add('was-validated');
+      }, false);
+      });
+      }, false);
+      })();
+  // New employee form inputs validation ends
+
+  });
 
 // Set focus for table search in employees directory
 document.getElementById("tableSearch").focus();
 // Focus for table search ends
+
+
+
+$(document).ready(function(){
+  var date_input=$('input[name="date"]'); //our date input has the name "date"
+  var container=$('.bootstrap-iso form').length>0 ? $('.bootstrap-iso form').parent() : "body";
+  var options={
+    format: 'mm/dd/yyyy',
+    container: container,
+    todayHighlight: true,
+    autoclose: true,
+  };
+  date_input.datepicker(options);
+});
